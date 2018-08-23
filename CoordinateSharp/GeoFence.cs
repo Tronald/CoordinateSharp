acdae8f554601161c83875e74dff9a6973ff3dfd
Justin Gielski <justin.gielski@gmail.com>
2018-08-22 23:11:46 -0700

    Update Properties

--- CoordinateSharp/GeoFence.cs
+++ CoordinateSharp/GeoFence.cs
@@ -130,6 +130,24 @@ namespace CoordinateSharp
         /// </summary>
         public class Point
         {
+
+            /// <summary>
+            /// Initialize empty point
+            /// </summary>
+            public Point()
+            {
+               
+            }
+            /// <summary>
+            /// Initialize point with defined Latitude and Longitude
+            /// </summary>
+            /// <param name="lat">Latitude (signed)</param>
+            /// <param name="lng">Longitude (signed)</param>
+            public Point(double lat, double lng)
+            {
+                Latitude = lat;
+                Longitude = lng;
+            }
             /// <summary>
             /// The longitude in degrees
             /// </summary>
