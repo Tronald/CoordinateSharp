62003d0df1e1389d591467dcaeb1f9a076f00f98
Justin Gielski <justin.gielski@gmail.com>
2019-08-15 08:57:41 -0700

    Exposed UTM_MGRS Signed Degree Converters

--- CoordinateSharp/UTM_MGRS/MGRS.Methods.cs
+++ CoordinateSharp/UTM_MGRS/MGRS.Methods.cs
@@ -342,6 +342,100 @@ namespace CoordinateSharp
         }
 
         /// <summary>
+        /// Creates a Signed Degree double[] object from an MGRS/NATO UTM Coordinate
+        /// </summary>
+        /// <param name="mgrs">MilitaryGridReferenceSystem</param>
+        /// <returns>Coordinate object</returns>
+        /// <example>
+        /// The following example creates (converts to) a geodetic Coordinate object based on a MGRS object.
+        /// <code>
+        /// MilitaryGridReferenceSystem mgrs = new MilitaryGridReferenceSystem("N", 21, "SA", 66037, 61982);
+        /// double[] sd = MilitaryGridReferenceSystem.MGRStoSignedDegree(mgrs);
+        /// Coordinate c = new Coordinate(sd[0],sd[1], new EagerLoad(false));
+        /// Console.WriteLine(c); //N 0º 33' 35.988" W 60º 0' 0.01"
+        /// </code>
+        /// </example>
+        public static double[] MGRStoSignedDegree(MilitaryGridReferenceSystem mgrs)
+        {
+            string latz = mgrs.LatZone;
+            string digraph = mgrs.Digraph;
+
+            char eltr = digraph[0];
+            char nltr = digraph[1];
+
+            string digraphLettersE = "ABCDEFGHJKLMNPQRSTUVWXYZ";
+            string digraphLettersN = "ABCDEFGHJKLMNPQRSTUV";
+            string digraphLettersAll = "";
+            for (int lt = 1; lt < 25; lt++)
+            {
+                digraphLettersAll += "ABCDEFGHJKLMNPQRSTUV";
+            }
+
+            var eidx = digraphLettersE.IndexOf(eltr);
+            var nidx = digraphLettersN.IndexOf(nltr);
+            if (mgrs.LongZone / 2.0 == Math.Floor(mgrs.LongZone / 2.0))
+            {
+                nidx -= 5;  // correction for even numbered zones
+            }
+
+            var ebase = 100000 * (1 + eidx - 8 * Math.Floor(Convert.ToDouble(eidx) / 8));
+            var latBand = digraphLettersE.IndexOf(latz);
+            var latBandLow = 8 * latBand - 96;
+            var latBandHigh = 8 * latBand - 88;
+
+            if (latBand < 2)
+            {
+                latBandLow = -90;
+                latBandHigh = -80;
+            }
+            else if (latBand == 21)
+            {
+                latBandLow = 72;
+                latBandHigh = 84;
+            }
+            else if (latBand > 21)
+            {
+                latBandLow = 84;
+                latBandHigh = 90;
+            }
+
+            var lowLetter = Math.Floor(100 + 1.11 * latBandLow);
+            var highLetter = Math.Round(100 + 1.11 * latBandHigh);
+
+            string latBandLetters = null;
+            int l = Convert.ToInt32(lowLetter);
+            int h = Convert.ToInt32(highLetter);
+            if (mgrs.LongZone / 2.0 == Math.Floor(mgrs.LongZone / 2.0))
+            {
+                latBandLetters = digraphLettersAll.Substring(l + 5, h + 5).ToString();
+            }
+            else
+            {
+                latBandLetters = digraphLettersAll.Substring(l, h).ToString();
+            }
+            var nbase = 100000 * (lowLetter + latBandLetters.IndexOf(nltr));
+            //latBandLetters.IndexOf(nltr) value causing incorrect Northing below -80
+            var x = ebase + mgrs.Easting;
+            var y = nbase + mgrs.Northing;
+            if (y > 10000000)
+            {
+                y = y - 10000000;
+            }
+            if (nbase >= 10000000)
+            {
+                y = nbase + mgrs.northing - 10000000;
+            }
+
+            var southern = nbase < 10000000;
+            UniversalTransverseMercator utm = new UniversalTransverseMercator(mgrs.LatZone, mgrs.LongZone, x, y);
+            utm.equatorial_radius = mgrs.equatorialRadius;
+            utm.inverse_flattening = mgrs.inverseFlattening;
+            double[] sd = UniversalTransverseMercator.ConvertUTMtoSignedDegree(utm);
+
+            return sd;
+        }
+
+        /// <summary>
         /// Default formatted MGRS string
         /// </summary>
         /// <returns>MGRS Formatted Coordinate String</returns>
