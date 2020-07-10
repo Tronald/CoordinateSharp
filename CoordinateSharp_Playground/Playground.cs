01ad32a21a12f7d720874daa7a3941a62492dd89
Justin Gielski <justin.gielski@gmail.com>
2020-07-09 17:09:56 -0700

    Updated Playground Project

---
+++ CoordinateSharp_Playground/Playground.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CoordinateSharp;
+using System.IO;
+using System.Timers;
+using System.Diagnostics;
+namespace CoordinateSharp_Playground
+{
+    class Playground
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        private static void Benchmark(Action act, int iterations, string s)
+        {
+            GC.Collect();
+            act.Invoke(); // run once outside of loop to avoid initialization costs
+            Stopwatch sw = Stopwatch.StartNew();
+            for (int i = 0; i < iterations; i++)
+            {
+                act.Invoke();
+            }
+            sw.Stop();
+            Console.Write(s + ": ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write((sw.ElapsedMilliseconds / iterations).ToString() + " ms");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine();
+        }
+    }
+
+   
+}
