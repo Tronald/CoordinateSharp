5f85556da9b267944ff1112a62ed9bd7d590b2ca
Justin Gielski <justin.gielski@gmail.com>
2020-07-12 17:59:08 -0700

    Add Parser Null or Empty Test

---
+++ CoordinateSharp_UnitTests/Coordinate_Parsers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text;
+using CoordinateSharp;
+namespace CoordinateSharp_UnitTests
+{
+    [TestClass]
+    public class Coordinate_Parsers
+    {
+        /// <summary>
+        /// Ensures coordinate does not parse null or empty.
+        /// </summary>
+        [TestMethod]
+        public void Does_Not_Parse_Null_Or_Empty_Strings()
+        {
+            Assert.ThrowsException<FormatException>(() => Coordinate.Parse(null));
+            Assert.ThrowsException<FormatException>(() => Coordinate.Parse(""));
+            Coordinate c;
+            Assert.IsFalse(Coordinate.TryParse(null, out c));
+            Assert.IsFalse(Coordinate.TryParse("", out c));
+        }
+    }
+}
