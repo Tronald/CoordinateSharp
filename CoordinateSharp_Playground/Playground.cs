9c0d1f24270217b549fa5d1de6c936af2efcb390
Justin Gielski <justin@signatgroup.com>
2020-12-18 12:10:25 -0800

    Clean playground

--- CoordinateSharp_Playground/Playground.cs
+++ CoordinateSharp_Playground/Playground.cs
@@ -21,31 +21,8 @@ namespace CoordinateSharp_Playground
     {    
         [STAThread]
         static void Main()
-        {
-            //89,-121 (2020-1-1) should return GV 9.08
-            // Get others to complete tests.
-
-
-            //NE TOO HANDLE LOCAL TIMES
-            //Play Here
-            Coordinate c = new Coordinate(89, -121, new DateTime(2020, 1, 1), new EagerLoad(false));
-            var m = new Magnetic(c,new Distance(0), DataModel.WMM2020);
-            Output.Output_Class_Values(m.MagneticFieldElements, OutputOption.Console);
-            Console.WriteLine();
-            Output.Output_Class_Values(m.SecularVariations, OutputOption.Console);
-            Console.WriteLine();
-            Output.Output_Class_Values(m.Uncertainty, OutputOption.Console);
-            Console.WriteLine();
-            //End
-            Console.ReadKey();
-           
-        }
-
-        private static void C_CoordinateChanged(object sender, EventArgs e)
-        {            
-            var c = (Coordinate)sender;
-            Magnetic m = new Magnetic(c, DataModel.WMM2020);
-            Console.WriteLine("DEC: " + m.MagneticFieldElements.Declination);
-        }
+        {        
+            Console.ReadKey();           
+        }      
     } 
 }
